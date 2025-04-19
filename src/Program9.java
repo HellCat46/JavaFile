baffb251f8ccc0202926ed74391266e9fea00f34
HellCat46 <hellcat2388@gmail.com>
2025-04-19 12:40:55 +0530

    Minor Changes

--- src/Program9.java
+++ src/Program9.java
@@ -3,7 +3,7 @@ import java.util.*;
 class Program9 {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter Year: ");
+        System.out.print("Enter Year: ");
         int year = sc.nextInt();
 
         if(year % 4 == 0 && year % 100 != 0 ){
